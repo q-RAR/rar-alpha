797adc642016b077ddf13650c8e4f9a55f7d903d
q-RAR <90776792+q-RAR@users.noreply.github.com>
2021-12-30 11:10:37 +0800

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -2,26 +2,20 @@ const donasibot = () => {
 return `DONASI BISA VIA BANK / E-MONEY
 
 *🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
+BCA ㅁ 873 5080 331
 	
 *💰 E-MONEY*
-
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ 082199399855
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 082199399855
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 082199399855
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama MUH IKRAR
+
+*📲 PULSA*
+	ㅁ 082199399855
 `
 	}
 
