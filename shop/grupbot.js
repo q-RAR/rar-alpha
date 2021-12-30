03ee15070a3871b0875999ecad34a0aa9f249ed1
q-RAR <90776792+q-RAR@users.noreply.github.com>
2021-12-30 11:13:05 +0800

    Update grupbot.js

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -2,14 +2,8 @@
 const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
-1. *Alphabot Support*
-_https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV_
-2. *Alphabot Support 2*
-_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
-3. *Alphabot Support 3*
-_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
-4. *Alphabot Support 4* 
-_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
+1. *k-RAR Support*
+_https://chat.whatsapp.com/JQFaWZYraKC8VSCz4OidkG_
 
 Jika ada link yang ke reset, silahkan hubungi
 owner untuk meminta link yang baru
